d6f55f6af05df2c9de439c85dc4a30e133609727
Henare Degan <henare.degan@gmail.com>
2012-07-24 13:53:19 +1000

    Create a function to get recurring contributions that are in a pending state

--- org.civicrm.ewayrecurring/process_recurring_eway_payments.php
+++ org.civicrm.ewayrecurring/process_recurring_eway_payments.php
@@ -32,13 +32,52 @@ define('PENDING_CONTRIBUTION_STATUS_ID', 2);
 chdir(CIVICRM_DIRECTORY);
 require 'civicrm.config.php';
 require 'api/api.php';
+require 'CRM/Contribute/DAO/ContributionRecur.php';
 
-// Get pending contributions
-$params = array(
-    'version' => 3,
-    // TODO: Statuses are customisable so this configuration should be read from the DB
-    'contribution_status_id' => 2
-);
+/**
+ * get_pending_recurring_contributions
+ *
+ * Gets recurring contributions that are in a pending state.
+ * These are for newly created recurring contributions and should
+ * generally be processed the same day they're created. These do not
+ * include the regularly processed recurring transactions.
+ *
+ * @return array An array of associative arrays containing contribution arrays & contribtion_recur objects
+ */
+function get_pending_recurring_contributions()
+{
+    // Get pending contributions
+    $params = array(
+        'version' => 3,
+        // TODO: Statuses are customisable so this configuration should be read from the DB
+        'contribution_status_id' => PENDING_CONTRIBUTION_STATUS_ID
+    );
+    $pending_contributions = civicrm_api('contribution', 'get', $params);
 
-$pending_contributions = civicrm_api('contribution', 'get', $params);
+    $result = array();
+
+    foreach ($pending_contributions['values'] as $contribution) {
+        // Only process those with recurring contribution records
+        if ($contribution['contribution_recur_id']) {
+            // Find the recurring contribution record for this contribution
+            $recurring = new CRM_Contribute_DAO_ContributionRecur();
+            $recurring->id = $contribution['contribution_recur_id'];
+
+            // Only process records that have a recurring record with
+            // a processor ID, i.e. an eWay token
+            if ($recurring->find(true) && $recurring->processor_id) {
+                // TODO: Return the same type of results
+                // This is a bit nasty, contribution is an array and
+                // contribution_recur is an object
+                $result[] = array(
+                    'contribution' => $contribution,
+                    'contribution_recur' => $recurring
+                );
+            }
+        }
+    }
+    return $result;
+}
+
+
 
