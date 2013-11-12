9d18c03bcb525ae75639cfd05ea3b7613c90d353
Chris Ward <chris@moatmedia.com.au>
2013-11-12 14:59:37 +1100

    Finally got api call working

--- api/EwayrecurringProcess.php
+++ api/EwayrecurringProcess.php
@@ -1,39 +1,7 @@
-<?php
-/*
- +--------------------------------------------------------------------+
- | CiviCRM                                                            |
- +--------------------------------------------------------------------+
- | Copyright Henare Degan (C) 2012                                    |
- +--------------------------------------------------------------------+
- |                                                                    |
- | CiviCRM is free software; you can copy, modify, and distribute it  |
- | under the terms of the GNU Affero General Public License           |
- | Version 3, 19 November 2007.                                       |
- |                                                                    |
- | CiviCRM is distributed in the hope that it will be useful, but     |
- | WITHOUT ANY WARRANTY; without even the implied warranty of         |
- | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
- | See the GNU Affero General Public License for more details.        |
- |                                                                    |
- | You should have received a copy of the GNU Affero General Public   |
- | License along with this program; if not, contact CiviCRM LLC       |
- | at info[AT]civicrm[DOT]org. If you have questions about the        |
- | GNU Affero General Public License or the licensing of CiviCRM,     |
- | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
- +--------------------------------------------------------------------+
-*/
-/**
- * eWayProcess API call
- *
- * @param array $params
- * @return array API result descriptor
- * @see civicrm_api3_create_success
- * @see civicrm_api3_create_error
- * @throws API_Exception
- */
-//function civicrm_api3_job_googleapps_sync($params) {
 
-function civicrm_api3_job_eway($params) {
+<?php
+
+function civicrm_api3_job_ewayrecurring_process($params) {
 
     // TODO: Remove hacky hardcoded constants
     // The full path to your CiviCRM directory
